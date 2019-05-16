98caa70b73e3171413ce3f5394d3aabf80a34901
Abraham Franzén <abraham.franzen@learnet.se>
2019-05-16 10:06:49 +0200

    last

---
+++ file.java
@@ -0,0 +1,11 @@
+import java.io bufferReader;
+import java.io.FileReader;
+
+public class Exempel {
+   public static void main (String [] args} {
+      String filename = "exempel txt";
+         Filereader fr = null;
+            try{         
+              FileReader; Fr = new FileReader: (Filename); {
+         } catch (FIleReader(filename);
+          }-
